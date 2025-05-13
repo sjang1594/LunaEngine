25ffd96f64e2959ce684dcd13b514c73de8a2a2d
Seungho Jang <sjang1594@gmail.com>
2025-05-13 09:22:14 +0900

    feat: define vulkandevice / backend

--- LunaEngine/src/LunaEngine/Renderer/Vulkan/Private/VulkanDevice.cpp
+++ LunaEngine/src/LunaEngine/Renderer/Vulkan/Private/VulkanDevice.cpp
@@ -1,55 +1,144 @@
 #include "LunaPCH.h"
 #include "LunaEngine/Renderer/Vulkan/Public/VulkanDevice.h"
+#include "Logger/Logger.h"
 
 namespace Luna
 {
 
-VulkanDevice::VulkanDevice(VkInstance instance) : _vkInstance(instance)
+VulkanDevice::~VulkanDevice()
 {
-    PickPhysicalDevice();
-    CreateLogicalDevice();
+    ShutDown();
 }
 
-VulkanDevice::~VulkanDevice()
+bool VulkanDevice::Initialize(VkInstance instance, VkSurfaceKHR surface)
+{
+    if (!PickPhysicalDevice(instance, surface))
+    {
+        LUNA_LOG_ERROR("Failed to pick a physical device");
+        return false;
+    }
+
+    if (!CreateLogicalDevice())
+    {
+        LUNA_LOG_ERROR("Failed to create logical device");
+        return false;
+    }
+
+    vkGetDeviceQueue(_device, _graphicsQueueFamily, 0, &_graphicsQueue);
+    vkGetDeviceQueue(_device, _presentQueueFamily, 0, &_presentQueue);
+    return true;
+}
+
+void VulkanDevice::ShutDown()
 {
     if (_device)
     {
         vkDestroyDevice(_device, nullptr);
+        _device = VK_NULL_HANDLE;
     }
 }
 
-void VulkanDevice::PickPhysicalDevice()
+bool VulkanDevice::PickPhysicalDevice(VkInstance instance, VkSurfaceKHR surface)
 {
-    VkResult res;
     uint32_t _physicalDeviceCount = 0; // # of gpu
-    res = vkEnumeratePhysicalDevices(_vkInstance, &_physicalDeviceCount, nullptr);
-    CheckIfVKFailed(res);
-    _physicalDevices.resize(_physicalDeviceCount);
-    res = vkEnumeratePhysicalDevices(_vkInstance, &_physicalDeviceCount, _physicalDevices.data());
-    CheckIfVKFailed(res);
-    for (const auto& device : _physicalDevices)
+    vkEnumeratePhysicalDevices(instance, &_physicalDeviceCount, nullptr);
+    if (_physicalDeviceCount == 0)
     {
-        VkPhysicalDeviceProperties props;
-        vkGetPhysicalDeviceProperties(device, &props);
-        if (props.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+        LUNA_LOG_ERROR("No Vulkan-Support GPUs with Vulkan support");
+        return false;
+    }
+
+    std::vector<VkPhysicalDevice> devices(_physicalDeviceCount);
+    
+    devices.resize(_physicalDeviceCount);
+    vkEnumeratePhysicalDevices(instance, &_physicalDeviceCount, devices.data());
+
+    for (const auto& device : devices)
+    {
+        if (FindQueueFamilies(device, surface))
         {
+            VkPhysicalDeviceProperties props;
+            vkGetPhysicalDeviceProperties(device, &props);
+            LUNA_LOG_INFO("Found GPU: %s", props.deviceName);
             _physicalDevice = device;
-            break;
+            return true;
         }
     }
-    CheckIfVKFailed(res);
+
+    return false;
 }
 
-void VulkanDevice::CreateLogicalDevice()
+bool VulkanDevice::CreateLogicalDevice()
 {
-    VkResult res;
-    VkDeviceCreateInfo createInfo{};
+    float queuePriority = 1.0f;
+    std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+    VkDeviceQueueCreateInfo queueCreateInfo {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = _graphicsQueueFamily;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+    queueCreateInfos.push_back(queueCreateInfo);
+
+    if (_graphicsQueueFamily != _presentQueueFamily)
+    {
+        queueCreateInfo.queueFamilyIndex = _presentQueueFamily;
+        queueCreateInfos.push_back(queueCreateInfo);
+    }
+
+    // TODO: Comments
+    VkPhysicalDeviceFeatures deviceFeatures {};
+    deviceFeatures.samplerAnisotropy = VK_TRUE;
+
+    VkDeviceCreateInfo createInfo {};
     createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    res = vkCreateDevice(_physicalDevice, &createInfo, nullptr, &_device);
-    CheckIfVKFailed(res);
+    createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+    createInfo.pQueueCreateInfos = queueCreateInfos.data();
+    createInfo.pEnabledFeatures = &deviceFeatures;
+
+    const char* deviceExtensions[] = { "VK_KHR_swapchain" };
+    createInfo.enabledExtensionCount = 1;
+    createInfo.ppEnabledExtensionNames = deviceExtensions;
+
+    if (vkCreateDevice(_physicalDevice, &createInfo, nullptr, &_device) != VK_SUCCESS)
+    {
+        LUNA_LOG_ERROR("Failed to create logical device");
+        return false;
+    }
+    return true;
 }
 
-void VulkanDevice::InitializeQueues()
+bool VulkanDevice::FindQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface)
 {
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    bool graphicsFound = false;
+    bool presentFound = false;
+
+    for (uint32_t i = 0; i < queueFamilies.size(); i++)
+    {
+        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            _graphicsQueueFamily = i;
+            graphicsFound = true;
+        }
+
+        VkBool32 presentSupport = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+        if (presentSupport) {
+            _presentQueueFamily = i;
+            presentFound = true;
+        }
+
+        if (graphicsFound && presentFound) {
+            return true;
+        }
+    }
+
+    return false;
 }
+
 }
--- LunaEngine/src/LunaEngine/Renderer/Vulkan/Public/VulkanDevice.h
+++ LunaEngine/src/LunaEngine/Renderer/Vulkan/Public/VulkanDevice.h
@@ -7,24 +7,30 @@ namespace Luna
 class VulkanDevice : public IRenderDevice
 {
 public:
-    VulkanDevice(VkInstance instance);
+    VulkanDevice() = default;
     ~VulkanDevice() override;
+    
+    bool Initialize(VkInstance instance, VkSurfaceKHR surface) override;
+    void ShutDown();
+    
     const char* GetDeviceName() const override { return "Vulkan"; }
     VkDevice GetDevice() const { return _device; }
     VkPhysicalDevice GetPhysicalDevice() const { return _physicalDevice; }
     VkQueue GetGraphicsQueue() const { return _graphicsQueue; }
     VkQueue GetPresentQueue() const { return _presentQueue; }
+    uint32_t GetGraphicsQueueFamily() const { return _graphicsQueueFamily; }
     
 private:
-    void PickPhysicalDevice();
-    void CreateLogicalDevice();
-    void InitializeQueues();
+    bool PickPhysicalDevice(VkInstance instance, VkSurfaceKHR surface);
+    bool CreateLogicalDevice();
+    bool FindQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface);
+
     
-    VkDevice                        _device = VK_NULL_HANDLE;
-    VkPhysicalDevice                _physicalDevice = VK_NULL_HANDLE; // gpu
-    vector<VkPhysicalDevice>        _physicalDevices;
-    VkInstance                      _vkInstance;
-    VkQueue                         _graphicsQueue = VK_NULL_HANDLE;
-    VkQueue                         _presentQueue = VK_NULL_HANDLE;
+    VkPhysicalDevice    _physicalDevice = VK_NULL_HANDLE;
+    VkDevice            _device = VK_NULL_HANDLE;
+    VkQueue             _graphicsQueue;
+    VkQueue             _presentQueue;
+    uint32_t            _graphicsQueueFamily;
+    uint32_t            _presentQueueFamily;
 };
 }
